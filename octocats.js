a1af8afdca9a49fc05d098216c4e38eabf96a619
brosenbusch <8brosenbusch@stpatrick.org>
2016-10-07 12:49:38 -0500

    I finished.

--- octocats.js
+++ octocats.js
@@ -1,14 +1,17 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
+function Octocat(){
+  this.name = n;
+  this.arms = a;
 
-
-
-
-
-
-
-
-
-
+  this.slap = function(){
+    for (b=0,b<this.arms,b++){
+      console.log ("SLAP");
+    }
+  }
+}
 
 //Write your 3 new octocat objects below here.
+var brrrrrcat = new Octocat("Brett",9);
+var tonyazzengacat = new Octocat("Tony Mazzenga",3);
+var tommycat = new Octocat("Tommy",11);
